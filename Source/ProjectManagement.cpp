dafa91e8a6e5f8234d455c8f301c0b3a0f5f581c
Nolan Strait <nlstrait@gmail.com>
2021-05-23 19:57:21 -0700

    flesh out ProjectManagement

--- Source/ProjectManagement.cpp
+++ Source/ProjectManagement.cpp
@@ -12,11 +12,23 @@
 
 #include "ProjectManagement.h"
 
-Layer::Layer(juce::File& file) : layerFile(file) {}
+
+//===================================== Layer =========================================
+
+Layer::Layer(juce::File file) : layerFile(file) {}
 
 bool Layer::operator==(const Layer &other) {
     return layerFile == other.layerFile;
 }
+
+juce::File& Layer::getFile() { return layerFile; }
+
+
+//===================================== Project =========================================
+
+// This seems like bad practice but was necessary to get working with juce::Array. Normally,
+// I would not create a default constructor like this. This should never be called. - Nolan
+Project::Project() : layers(), mixdownFile(juce::File()) {}
 
 Project::Project(juce::File& mixdownFile) : mixdownFile(mixdownFile) {
     if (mixdownFile.isDirectory()) throw "Invalid mixdownFile given; mixdownFile is a directory";
@@ -31,11 +43,33 @@ Project::Project(juce::File& mixdownFile) : mixdownFile(mixdownFile) {
     }
 }
 
+juce::String Project::getName() {
+    return mixdownFile.getFileNameWithoutExtension();
+}
+
+juce::File& Project::getMixdownFile() { return mixdownFile; }
+
+juce::File Project::getLayerDirectory() {
+    return mixdownFile.withFileExtension(juce::StringRef());
+}
+
+juce::File Project::createNewLayer() {
+    juce::File layerFile = getLayerDirectory().getNonexistentChildFile("layer_" + std::to_string(getNumLayers() + 1), ".wav");
+    layers.add(Layer(layerFile));
+    return layerFile;
+}
+
+int Project::getNumLayers() { return layers.size(); }
+
+
+//===================================== ProjectManagement =========================================
+
 juce::Array<Project> ProjectManagement::getAllProjectsInFolder(juce::File &mixdownFolder) {
     juce::Array<Project> projects;
     juce::Array<juce::File> children = mixdownFolder.findChildFiles(juce::File::findFiles, false);
     for (juce::File child : children) {
-        projects.add(Project(child));
+        if (child.getFileExtension().equalsIgnoreCase(".wav"))
+            projects.add(Project(child));
     }
     return projects;
 }
--- Source/ProjectManagement.h
+++ Source/ProjectManagement.h
@@ -15,14 +15,21 @@
 
 class Layer {
 public:
-    Layer(juce::File& file);
+    Layer(juce::File file);
     bool operator==(const Layer& other);
+    
+    juce::File& getFile();
+    
 private:
-    juce::File& layerFile;
+    juce::File layerFile;
 };
+
 
 class Project {
 public:
+    
+    Project();
+    
     /**
      Create a new Project.
      @throws Invalid mixdownFile exception if mixdownFile is a directory.
@@ -30,13 +37,41 @@ public:
      */
     Project(juce::File& mixdownFile);
     
+    /**
+     Return the name of this Project, which is based on its mixdown.
+     */
+    juce::String getName();
+    
+    /**
+     Retrieve this Project's mixdown file.
+     @return A mixdown file.
+     */
+    juce::File& getMixdownFile();
+    
+    /**
+     Retrieve the directory where this Project's Layers live.
+     @return A directory.
+     */
+    juce::File getLayerDirectory();
+    
+    /**
+     Creates and returns a new layer for this Project.
+     */
+    juce::File createNewLayer();
+    
+    /**
+     Return the number of Layers within this Project.
+     */
+    int getNumLayers();
+    
     // The layers which this Project contains. Open for manipulation from outsiders,
     // at least for now, to make for ease of use.
     juce::Array<Layer> layers;
     
 private:
-    juce::File& mixdownFile;
+    juce::File mixdownFile;
 };
+
 
 /**
  Utilities for generating and managing collections of projects.
@@ -46,6 +81,11 @@ public:
     /**
      Generate Projects from a given mixdown folder. Assumes that the folder specified has mixdown audio files in its root
      and layers for each project in subfolders with names identicle to their associated mixdown file (minus the file extension).
+     
+     For example, with a mixdown named "mixd.wav", we expect a subfolder within "mixd.wav"'s containing folder to be named
+     "mixd/" and contain the audio layers for the mixd project. If there is no such subfolder, it is assumed that this project
+     has no layers (yet).
+     
      @param mixdownFolder   The folder to generate mixdowns from.
      @return    A list of Projects found in given directory.
      */
